7a8cea5517a567b1737aa253927db6b6c7dbe6a7
Abdukhamitov <59801257+Abdukhamitov@users.noreply.github.com>
2021-02-06 12:33:04 +0100

    New

---
+++ Cubex/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameManager2 gameManager;
+    void OnTriggerEnter()
+    {
+        gameManager.CompleteLevel();
+    }
+}
---
+++ Cubex/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 200f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //add a ForwardForce
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        //move right
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        //move left
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager2>().EndGame();
+        }
+
+
+    }
+}
